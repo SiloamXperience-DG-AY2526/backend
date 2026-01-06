9666d336f64f17c38300f3ec8ad3b55f34db7e15
Jun Wei <156562637+junwei07@users.noreply.github.com>
2026-01-06 17:40:53 +0800

    Resolve merge conflicts

--- src/authorisation/permissions/index.ts
+++ src/authorisation/permissions/index.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import { financePermissions } from './finance.permissions';
-import { projectPermissions } from './project.permissions';
-import { profilePermissions } from './profile.permissions';
-=======
 import { donationPermissions } from './donation.permissions';
->>>>>>> main
 import { PermissionHandler } from '../types';
 
 export type PermissionsMap = Record<string, PermissionHandler>;
@@ -16,12 +10,6 @@ export type Permission = keyof typeof PERMISSIONS;
  * Note: permission names should be defined as 'resource:action:own(optional)'
  */
 export const PERMISSIONS = {
-<<<<<<< HEAD
-  ...financePermissions,
-  ...projectPermissions,
-  ...profilePermissions,
-=======
   ...donationPermissions,
->>>>>>> main
   // DEV: Add other permission groups here
 } satisfies PermissionsMap;
