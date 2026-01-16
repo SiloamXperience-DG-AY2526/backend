eee1d4d3eb3fa14ce35d8263a4480a7287c7d937
Jun Wei <156562637+junwei07@users.noreply.github.com>
2026-01-16 21:33:06 +0800

    Profile changes (#124)
    * Extra package.json
    
    * Add handling of staff profile
    
    * Remove extra route
    
    * Remove extra return

--- src/models/general.model.ts
+++ src/models/general.model.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../lib/prisma';
 import { PartnerFeedbackType, ProjectApprovalStatus } from '@prisma/client';
+import { StaffProfile } from '../schemas/user';
+import { BadRequestError } from '../utils/errors';
 import { PMPublicSelect } from './projectionSchemas/user.projections';
 
 
@@ -114,3 +116,43 @@ export const getPeerFeedbackByManager = async (userId: string) => {
     },
   });
 };
+
+export const getStaffProfile = async (
+  userId: string
+): Promise<StaffProfile | null> => {
+  const staff = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      firstName: true,
+      lastName: true,
+      title: true,
+      email: true,
+    },
+  });
+
+  if (!staff) throw new BadRequestError('Error retrieving staff profile');
+
+  return {
+    firstName: staff.firstName,
+    lastName: staff.lastName,
+    email: staff.email,
+    title: staff.title ?? undefined,
+  };
+};
+
+export async function updateStaffProfile(
+  userId: string,
+  newStaffProfile: StaffProfile
+) {
+
+  await prisma.user.update({
+    where: { id: userId },
+    data: {
+      ...newStaffProfile,
+    },
+  });
+
+  const updatedProfile = await getStaffProfile(userId);
+  
+  return updatedProfile;
+}
